fb311fae667807da33452bb81f2bf9d8f58c48d7
Shumaila-Rehman <pathan369khan@gmail.com>
2024-02-29 22:16:33 +0500

    datatype

---
+++ example.js
@@ -0,0 +1,16 @@
+// No 1.. string
+var myName = "shumaila";
+console.log(myName);
+// No 2.. Number
+var myAge = 20;
+var finalAge = myAge + 5;
+console.log("my age will be ", finalAge);
+//  N0. 3 Boolean
+var IsFollow = true;
+console.log(IsFollow);
+// No 4. Null
+var Empty = null;
+console.log(Empty);
+// No 5. Undefined
+var box2 = undefined;
+console.log(box2);
---
+++ example.ts
@@ -0,0 +1,27 @@
+// No 1.. string
+let myName : string = "shumaila" ;
+console.log(myName);
+
+// No 2.. Number
+const myAge : number = 20;
+const finalAge : number = myAge + 5;
+console.log("my age will be " ,finalAge);
+
+//  N0. 3 Boolean
+ let IsFollow : boolean = true;
+ console.log(IsFollow);
+
+ // No 4. Null
+
+const Empty : null = null;
+console.log(Empty);
+
+// No 5. Undefined
+let box2 = undefined;
+console.log(box2);
+
+
+
+
+
+
